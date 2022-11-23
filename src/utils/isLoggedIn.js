d7efbb15ffcd5e8ae37fcaea81f9888f82cb21ea
Joshua Laurence <laurence11@fb.com>
2022-11-23 16:18:11 +0000

    Added front end utilities

---
+++ src/utils/isLoggedIn.js
@@ -0,0 +1,21 @@
+import {retrieve} from "./retrieve.js"
+
+async function isLoggedIn() {
+    const blogToken = localStorage.getItem("blogToken")
+    if (blogToken !== null) {
+        const loggedIn = await retrieve("loginStatus", "POST", {token: blogToken})
+        if (loggedIn.success) {
+            return {
+                loggedIn: true,
+                blogAddress: loggedIn.blogAddress
+            }
+        } else {
+            return {
+                loggedIn: false,
+                error: loggedIn.error
+            }
+        }
+    }
+}
+
+export default isLoggedIn;
