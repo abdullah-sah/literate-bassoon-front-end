095a9781edbba68c678edd5414f4fb822335c52e
Joshua Laurence <laurence11@fb.com>
2022-11-24 16:28:35 +0000

    Fixed retrieve function

--- src/utils/retrieve.js
+++ src/utils/retrieve.js
@@ -1,29 +1,27 @@
-async function retrieve(urlEnd, method, body={}) {
-    try {
-        let result;
-        if (method === "GET") {
-            result = await fetch(`http://localhost:5001/${urlEnd}`, {
-            method: method,
-            headers: {
-                "Content-Type": "application/json"
-            }})
-        } else {
-            result = await fetch(`http://localhost:5001/${urlEnd}`, {
-                method: method,
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: body
-            })
-        }
-        const data = await result.json()
-        return data
-    } catch (error) {
-        return {
-            success: false,
-            error: error.message
-        }
-    }
+async function retrieve(urlEnd, method, body = null) {
+	try {
+		let reqObj = {
+			method: method,
+			headers: {
+				"Content-Type": "application/json",
+			}
+		}
+
+		if(body !== null){
+			reqObj['body'] = body;
+		}
+
+		const result = await fetch(`http://localhost:5001/${urlEnd}`, reqObj);
+		const data = await result.json();
+		return data;
+	} catch (error) {
+		return {
+			success: false,
+			error: error.message,
+		};
+	}
 }
+
+export default retrieve;
 
 export default retrieve;
